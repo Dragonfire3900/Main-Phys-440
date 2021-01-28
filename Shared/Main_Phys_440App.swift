9e9532cbe2d92799b26904fdd47a36572fa5cdc6
Joel Kelsey <tangentialcubic@gmail.com>
2021-01-28 12:37:08 -0600

    Initial Commit

---
+++ Shared/Main_Phys_440App.swift
@@ -0,0 +1,17 @@
+//
+//  Main_Phys_440App.swift
+//  Shared
+//
+//  Created by Joel Kelsey on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Main_Phys_440App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
