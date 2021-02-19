63ff9e27ae4a1d2885bdbd8a4bcb8e7890349b4f
Joel Kelsey <tangentialcubic@gmail.com>
2021-02-19 17:33:50 -0600

    Everything will be okay just make sure you keep working Joel

---
+++ Shared/Box.swift
@@ -0,0 +1,30 @@
+//  Describes the m-dimensional box class which is used in many different situations
+//
+//  Box.swift
+//  Main Phys 440
+//
+//  Created by Joel Kelsey on 2/19/21.
+//
+
+import Foundation
+
+class Box {
+    //Variables
+    private var corner1: Array<CGFloat> //Needs to have the same count as c2
+    private var corner2: Array<CGFloat>
+    
+    //Constructors
+    init() {
+        //The designed constructor of this class
+        corner1 = Array()
+        corner2 = Array()
+    }
+    
+    //Setters
+    
+    //Getters
+    
+    //Calculators
+    
+    //Operators
+}
---
+++ Shared/IntegMeths.swift
@@ -0,0 +1,8 @@
+//
+//  IntegMeths.swift
+//  Main Phys 440
+//
+//  Created by Joel Kelsey on 2/19/21.
+//
+
+import Foundation
