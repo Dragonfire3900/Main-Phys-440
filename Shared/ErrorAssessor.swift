845cbdfbb511c0f06c1379cd671cfa70bffb27dc
Dragonfire3900 <tangentialcubic@gmail.com>
2021-02-26 13:14:31 -0600

    Started to work on hw 2

--- Shared/ErrorAssessor.swift
+++ Shared/ErrorAssessor.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+import SwiftUI
+import CorePlot
+
+class ErrorAssessor: NSObject, ObservableObject {
+    @Published var plotData = [plotDataType]()
+    @Published var plotParameters: ChangingPlotParameters = ChangingPlotParameters()
+    @Published var calculatedText = ""
+
+    @Published var pointNumber = 0.0
+
+    
+}
