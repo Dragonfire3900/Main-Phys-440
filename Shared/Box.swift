8cf340ab5397be02f5b5c05fcb45778e8bd86125
Joel Kelsey <tangentialcubic@gmail.com>
2021-03-05 17:40:27 -0600

    I'm tired and sad. Guess that's nothing different though

--- Shared/Box.swift
+++ Shared/Box.swift
@@ -10,20 +10,22 @@ import Foundation
 
 //Possible Box Errors
 enum BoxErrors: Error {
-    
+    case IncorrectDims //When the number of sides
 }
 
 class Box {
     //Variables
-    private var cor1: Array<CGFloat> = [CGFloat]()//Needs to have the same count as c2
+    private var cor1: Array<CGFloat> = [CGFloat]() //Needs to have the same count as c2
     private var cor2: Array<CGFloat> = [CGFloat]()
     
     //Constructors
-    init(newCor1: Array<CGFloat>, newCor2: Array<CGFloat>) {
+    init(newCor1: Array<CGFloat>, newCor2: Array<CGFloat>) throws {
         //The designed constructor of this class
         if (newCor1.count == newCor2.count) {
             self.cor1 = newCor1
             self.cor2 = newCor2
+        } else {
+            throw BoxErrors.IncorrectDims
         }
     }
     
