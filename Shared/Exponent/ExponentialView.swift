ed50d00e98e54ec268da9cece59167556b57fbff
Joel kelsey <tangentialcubic@gmail.com>
2022-02-04 17:48:54 -0600

    Started the Exponential problem

---
+++ Shared/Exponent/ExponentialView.swift
@@ -0,0 +1,20 @@
+//
+//  ExponentialView.swift
+//  Main Phys 440
+//
+//  Created by Joel Kelsey on 2/4/22.
+//
+
+import SwiftUI
+
+struct ExponentialView: View {
+    var body: some View {
+        Text("I do the exponentials")
+    }
+}
+
+struct ExponentialView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExponentialView()
+    }
+}
