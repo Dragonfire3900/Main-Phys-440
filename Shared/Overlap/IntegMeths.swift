84f138f6d66a353d9f3e5ea3bb8dbbecacca0f56
Joel kelsey <tangentialcubic@gmail.com>
2022-02-17 16:52:40 -0600

    Added in the view files for the overlap integral calculation and the snowflake display

--- Shared/Overlap/IntegMeths.swift
+++
@@ -1,8 +0,0 @@
-//
-//  IntegMeths.swift
-//  Main Phys 440
-//
-//  Created by Joel Kelsey on 2/19/21.
-//
-
-import Foundation
---
+++ Shared/Snowflake/SnowView.swift
@@ -0,0 +1,20 @@
+//
+//  SnowView.swift
+//  Main Phys 440
+//
+//  Created by Joel Kelsey on 2/17/22.
+//
+
+import SwiftUI
+
+struct SnowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SnowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SnowView()
+    }
+}
