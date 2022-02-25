f8e78a6b644df77e87e7eca0d830c1dd436b3916
Joel kelsey <tangentialcubic@gmail.com>
2022-02-24 20:50:13 -0600

    Got the simulator view to draw both boxes and the particles in the simulation

--- Shared/Absorption/AbsSimView.swift
+++ Shared/Absorption/AbsSimView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct AbsSimView: View {
     let pointArr: [ColorPoint]
-//    let lineArr:
+    let detectBox: Box
     
     var body: some View {
-        Rectangle() //Background
-            .fill(Color.white)
+        ZStack {
+            Rectangle() //Background
+                .fill(Color.white)
+            
+            Rectangle() //The detector box
+                .stroke(Color.black)
+                .offset(x: detectBox.getCenter(dimNum: 0), y: detectBox.getCenter(dimNum: 1))
+                .frame(width: detectBox.getLength(dimNum: 0), height: detectBox.getLength(dimNum: 1))
+            
+            ForEach(pointArr) { point in
+                Circle()
+                    .fill(point.col)
+                    .offset(x: point.x, y: point.y)
+                    .frame(width: 4.0, height: 4.0)
+            }
+        }
     }
 }
 
 struct AbsSimView_Previews: PreviewProvider {
     static var previews: some View {
-        AbsSimView(pointArr: [])
+        AbsSimView(pointArr: [], detectBox: Box(newDims: [10.0, 10.0], newCen: [0.0, 0.0]))
     }
 }
