c8a495ca444cd5cda524853f5d86de4e9b699864
Joel kelsey <tangentialcubic@gmail.com>
2022-03-04 16:27:03 -0600

    Expanded the protocols to include a type of storing function

--- Shared/Multiuse/MathProtocols.swift
+++ Shared/Multiuse/MathProtocols.swift
@@ -8,22 +8,37 @@
 import Foundation
 
 /// A type of math function which is only defined on a specific domain
-protocol RestrictedFunc {
+protocol funcDomRestriction: Sequence {
+    var domain: funcDomain { get }
     
+    func makeIterator() -> funcDomainIterator
 }
 
 /// A type of math function which maps a bunch of values to a single value
 protocol Projector {
-    /// Evaulate the function at some x-coordinate
+    /// Evaulate the function at some coordinates
     /// - Returns: The value of this mathematical function at that point
-    func Eval(vals: [Double]) -> Double
+    func Eval(val: Double) -> Double?
 }
 
 /// A type of math function which is recurssive in nature
-protocol MathRecursive: Sequence, IteratorProtocol {
+protocol MathRecursive: Sequence {
     /// The initial point which this recursive function has information about
-    let initPt: [Double]
+    var initPt: [Double] { get }
     
     /// The initial value at that particular point
-    let initVal: Double
+    var initVal: Double { get }
 }
+
+/// A type of function which stores its values along the way and has some form of interpolation if needed
+protocol StoredFunc {
+    var storedVals: [Double] { get set }
+    
+    func Interp(xVal: Double) -> Double
+    
+    func Interp(befInd: Int, xVal: Double) -> Double
+}
+
+
+//The protocols we actually use
+protocol RestrictProject: Projector, funcDomRestriction {}
