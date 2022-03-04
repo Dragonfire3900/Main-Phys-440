94399b9c2e2c1162154bfbd29a568acfdd91f518
Joel kelsey <tangentialcubic@gmail.com>
2022-03-04 16:28:30 -0600

    Expanded the function domains to include a boolean isIn and make them subscriptable

--- Shared/Multiuse/GenMath.swift
+++ Shared/Multiuse/GenMath.swift
@@ -13,6 +13,13 @@ class funcDomain: Sequence {
     let stepSize: Double
     let count: Int
     
+    subscript(index: Int) -> Double {
+        get {
+            //TODO: Guard this
+            return self.start + Double(index) * self.stepSize
+        }
+    }
+    
     init(nStart: Double, nEnd: Double, nStepSize: Double) {
         //TODO: Actually guard on this initializer
         self.start = nStart
@@ -26,14 +33,27 @@ class funcDomain: Sequence {
         return funcDomainIterator(self)
     }
     
+    /// Returns the index right before the double if it's in the range
+    /// - Parameter of: the double which you would like the index before of
+    /// - Returns: The index right before this double value within the range
     func index(of: Double) -> Int? {
-        let potIndex = Int(round((of - self.start) / self.stepSize))
+        let potIndex = Int(floor((of - self.start) / self.stepSize))
         
-        guard (potIndex > self.count) else {
+        guard (potIndex > self.count || potIndex < 0) else {
             return nil
         }
         
         return potIndex
+    }
+    
+    /// Returns if the double is within the domain
+    /// - Parameter val: The x-value where you would like to test
+    /// - Returns: If the test value is within the domain or not
+    func isIn(val: Double) -> Bool {
+        if (val >= start && val <= end) {
+            return true
+        }
+        return false
     }
 }
 
