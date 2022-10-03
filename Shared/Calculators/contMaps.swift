755adfece2a98ea3816f6ba36811141bda9e33d5
Dragonfire3900 <tangentialcubic@gmail.com>
2022-10-03 11:14:55 -0500

    Moved over the rung outta algorithm

--- Shared/Calculators/contMaps.swift
+++ Shared/Calculators/contMaps.swift
@@ -10,6 +10,7 @@ import Foundation
 
 protocol shroeMap: genericMap {
     var hBar2: T { get }
+    var pos: T { get set }
     
     func makeIterator(iterNum: Int, reset: Bool, stepSize: Double) -> genIter
     
@@ -24,7 +25,28 @@ extension shroeMap {
     var hBar2: T { return 7.63 }
     
     func next(time: T) -> [T] {
+        var tmpVal = currVal
+        let k0 = time * firstDeriv(val: tmpVal, pos: self.pos)
+        let l0 = time * secondDeriv(val: tmpVal, pos: self.pos)
         
+        tmpVal = [currVal[0] + k0 * 0.5, currVal[1] + l0 * 0.5]
+        let k1 = time * firstDeriv(val: tmpVal, pos: self.pos + time * 0.5)
+        let l1 = time * secondDeriv(val: tmpVal, pos: self.pos + time * 0.5)
+        
+        tmpVal = [currVal[0] + k1 * 0.5, currVal[1] + l1 * 0.5]
+        let k2 = time * firstDeriv(val: tmpVal, pos: self.pos + time * 0.5)
+        let l2 = time * secondDeriv(val: tmpVal, pos: self.pos + time * 0.5)
+        
+        tmpVal = [currVal[0] + k2 * 0.5, currVal[1] + l2 * 0.5]
+        let k3 = time * firstDeriv(val: tmpVal, pos: self.pos + time)
+        let l3 = time * secondDeriv(val: tmpVal, pos: self.pos + time)
+        
+        currVal[0] = currVal[0] + (1.0 / 6.0) * (k0 + 2*k1 + 2*k2 + k3)
+        currVal[1] = currVal[1] + (1.0 / 6.0) * (l0 + 2*l1 + 2*l2 + l3)
+        
+        self.pos += time
+        
+        return currVal
     }
     
     func makeIterator(iterNum: Int, reset: Bool, stepSize: Double) -> genIter {
